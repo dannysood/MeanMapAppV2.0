8d2a4a39b9897dac050ade2870676d8e381e31bd
Pulkit Sood <soodpulkit21@gmail.com>
2020-08-10 04:03:50 +0530

    fix config

--- app/config.js
+++ app/config.js
@@ -1,24 +1,12 @@
 // Sets the MongoDB Database options
 
-const userBuffer = new Buffer(process.env.DATABASE_USER, 'base64')
-const user = userBuffer.toString('ascii');
-const passwordBuffer = new Buffer(process.env.DATABASE_PASSWORD, 'base64')
-const password = passwordBuffer.toString('ascii');
-const hostBuffer = new Buffer(process.env.DATABASE_HOST, 'base64')
-const host = hostBuffer.toString('ascii');
-const dbnameBuffer = new Buffer(process.env.DATABASE_NAME, 'base64')
-const dbname = dbnameBuffer.toString('ascii');
-const portBuffer = new Buffer(process.env.DATABASE_PORT, 'base64')
-const port = portBuffer.toString('ascii');
-
-
 module.exports = {
 
     bitnami:
     {
         name: "MongoDB Service",
-        url: "mongodb://" + user + ":" + password + "@" + host + "/" + dbname,
-        port: port
+        url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
     }
 
 };
