96b0f808b30e02d9d7b279b6f08b167d99e982a9
Pulkit Sood <soodpulkit21@gmail.com>
2020-08-10 03:41:35 +0530

    temporarily o=commit logs

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ var app             = express();
 // Express Configuration
 // -----------------------------------------------------
 // Sets the connection to MongoDB
+const userBuffer = new Buffer(process.env.DATABASE_USER, 'base64')
+const user = userBuffer.toString('ascii');
+const passwordBuffer = new Buffer(process.env.DATABASE_PASSWORD, 'base64')
+const password = passwordBuffer.toString('ascii');
+const hostBuffer = new Buffer(process.env.DATABASE_HOST, 'base64')
+const host = hostBuffer.toString('ascii');
+const dbnameBuffer = new Buffer(process.env.DATABASE_NAME, 'base64')
+const dbname = dbnameBuffer.toString('ascii');
+const portBuffer = new Buffer(process.env.DATABASE_PORT, 'base64')
+const port = portBuffer.toString('ascii');
+console.log("###############@@@@@@@@@@@@@@@@@@@@@@@@@", {
+    name: "MongoDB Service",
+    url: "mongodb://" + user + ":" + password + "@" + host + "/" + dbname,
+    port: port
+});
+
 mongoose.connect(database.bitnami.url);
 
 // Logging and Parsing
