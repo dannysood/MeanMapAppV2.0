1cc4dafe241ae51ab6c30e6525d65d28e45fa944
Pulkit Sood <soodpulkit21@gmail.com>
2020-08-10 03:32:17 +0530

    update env config

--- app/config.js
+++ app/config.js
@@ -5,8 +5,8 @@ module.exports = {
     bitnami:
     {
         name: "MongoDB Service",
-        url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
-        port: process.env.DATABASE_PORT
+        url: "mongodb://" + Buffer(process.env.DATABASE_USER, 'base64').toString('ascii') + ":" + Buffer(process.env.DATABASE_PASSWORD, 'base64').toString('ascii') + "@" + Buffer(process.env.DATABASE_HOST, 'base64').toString('ascii') + "/" + Buffer(process.env.DATABASE_NAME, 'base64').toString('ascii'),
+        port: Buffer(process.env.DATABASE_PORT, 'base64').toString('ascii')
     }
 
 };
